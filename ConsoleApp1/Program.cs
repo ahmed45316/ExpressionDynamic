268fc9fbc9143386ad068914ad2b272a8b7e5c14
ahmed <mido44104@gmail.com>
2019-10-13 04:56:25 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using ExpressionGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>()
+            {
+                new Person(){Id=1,Name="ahmed",HiringDate=DateTime.Parse("2015/01/01")},
+                new Person(){Id=2,Name="ALI",HiringDate=DateTime.Parse("2015/07/01")},
+                new Person(){Id=3,Name="Mourad",HiringDate=DateTime.Parse("2014/08/08")},
+                new Person(){Id=4,Name="Samir",HiringDate=DateTime.Parse("2017/08/01")},
+                new Person(){Id=5,Name="ahmed",HiringDate=DateTime.Parse("2019/01/01")},
+                new Person(){Id=6,Name="ahmed",HiringDate=DateTime.Parse("2018/01/08")},
+                new Person(){Id=7,Name="ahmed",HiringDate=DateTime.Parse("2015/05/05")},
+            };
+
+            var builder = new FilterBuilder();
+            var filter = new Person() {Name="m", HiringDate = DateTime.Parse("2014/08/08") };
+            var x=  builder.GetExpression<Person, Person>(filter);
+            var test = people.Where(x.Compile()).ToList();
+        }
+    }
+  
+    public class Person
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? HiringDate { get; set; }
+    }   
+}
---
+++ ExpressionGenerator/FilterBuilder.cs
@@ -0,0 +1,52 @@
+﻿using ExpressionGenerator.Common;
+using LinqKit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace ExpressionGenerator
+{
+    public class FilterBuilder
+    {
+        public Expression<Func<T, bool>> GetExpression<T,U>(U filterDto) where T : class
+        {
+            var predicate = PredicateBuilder.New<T>(true);
+            var param = Expression.Parameter(typeof(T), "x");
+            var columns = typeof(U).GetProperties().Select(x => new {type=x.PropertyType, property = x.Name, value = x.GetValue(filterDto) }).Where(x => x.value != null).ToList();
+            foreach (var column in columns)
+            {
+                var member = Expression.Property(param, column.property);
+                var constant = GetValueExpression(column.property, column.value, param);
+                if (column.type == typeof(string))
+                {
+                   var exp = Expression.Lambda<Func<T, bool>>( member.StartWith(constant),param);
+                   predicate = predicate.And(exp);
+                }
+                else
+                {
+                    var exp = Expression.Equal(member, constant);
+                    predicate.And(Expression.Lambda<Func<T, bool>>(exp, param));
+                }
+            }
+
+            return predicate;
+        }
+        private static UnaryExpression GetValueExpression(string propertyName, object val, ParameterExpression param)
+        {
+            var member = Expression.Property(param, propertyName);
+            var propertyType = ((PropertyInfo)member.Member).PropertyType;
+            var converter = TypeDescriptor.GetConverter(propertyType);
+
+            if (!converter.CanConvertFrom(typeof(string)))
+                throw new NotSupportedException();
+
+            var constant = Expression.Constant(val);
+            return Expression.Convert(constant, propertyType);
+        }
+        
+    }
+}
